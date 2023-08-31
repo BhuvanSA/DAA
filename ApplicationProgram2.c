1d8dd8ef74db239d976b9f2063c63b50acbd5a6f
Bhuvan S A <bhuvansaraj2003@gmail.com>
2023-08-31 09:24:51 +0530

    v0.4

--- ApplicationProgram2.c
+++ ApplicationProgram2.c
@@ -1,91 +1,85 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// Function to merge two subarrays
-void merge(char **arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+// Merge two subarrays of arr[]
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r) {
+  int i, j, k;
+  int n1 = m - l + 1;
+  int n2 = r - m;
 
-    // Create temporary arrays
-    char *L = (char *)malloc(n1 * sizeof(char *));
-    char *R = (char *)malloc(n2 * sizeof(char *));
+  // Create temporary arrays
+  int L[n1], R[n2];
 
-    // Copy data to temporary arrays L[] and R[]
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
+  // Copy data to temporary arrays L[] and R[]
+  for (i = 0; i < n1; i++)
+    L[i] = arr[l + i];
+  for (j = 0; j < n2; j++)
+    R[j] = arr[m + 1 + j];
 
-    // Merge the temporary arrays back into arr[left..right]
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (strcmp(L[i], R[j]) <= 0) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+  // Merge the temporary arrays back into arr[l..r]
+  i = 0;
+  j = 0;
+  k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
     }
+    k++;
+  }
 
-    // Copy the remaining elements of L[], if any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+  // Copy the remaining elements of L[], if any
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
 
-    // Copy the remaining elements of R[], if any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-
-    // Free temporary arrays
-    free(L);
-    free(R);
+  // Copy the remaining elements of R[], if any
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
-// Function to perform merge sort
-void mergeSort(char **arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+// Merge sort function
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+
+    // Sort first and second halves
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    // Merge the sorted halves
+    merge(arr, l, m, r);
+  }
 }
 
 int main() {
-    int n;
-    printf("Enter the number of mobile numbers: ");
-    scanf("%d", &n);
+  int n;
+  printf("Enter the number of mobile numbers: ");
+  scanf("%d", &n);
 
-    // Allocating memory for an array of strings
-    char *mobileNumbers = (char *)malloc(n * sizeof(char *));
-    for (int i = 0; i < n; i++) {
-        mobileNumbers[i] = (char *)malloc(20 * sizeof(char));
-        printf("Enter mobile number %d: ", i + 1);
-        scanf("%s", mobileNumbers[i]);
-    }
+  int mobileNumbers[n];
+  printf("Enter the mobile numbers:\n");
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &mobileNumbers[i]);
+  }
 
-    // Sorting the mobile numbers using merge sort
-    mergeSort(mobileNumbers, 0, n - 1);
+  mergeSort(mobileNumbers, 0, n - 1);
 
-    // Displaying the sorted mobile numbers
-    printf("Sorted Mobile Numbers:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", mobileNumbers[i]);
-    }
+  printf("Sorted mobile numbers:\n");
+  for (int i = 0; i < n; i++) {
+    printf("%d\n", mobileNumbers[i]);
+  }
 
-    // Freeing allocated memory
-    for (int i = 0; i < n; i++) {
-        free(mobileNumbers[i]);
-    }
-    free(mobileNumbers);
-
-    return 0;
+  return 0;
 }
